1a5c0a7b35fe6567fde45733f84622a2cfec8ab1
Jeff Ross <jross323@gmail.com>
2018-11-24 16:42:28 -0800

    lots of front-end updates

--- ui/book.js
+++ ui/book.js
@@ -1,18 +1,18 @@
 const urlParams = new URLSearchParams(window.location.search);
 const bookID = urlParams.get("bookID");
 
-fetch(`https://localhost:44344/api/books/${bookID}`)
-    .then(response => response.json())
-    .then(populateData);
-
-// document.getElementById("saveButton").addEventListener("click", saveData); 
+if (bookID > 0) {
+    fetch(`https://localhost:44344/api/books/${bookID}`)
+        .then(response => response.json())
+        .then(populateForm);
+}
 
 document.getElementById("bookForm").addEventListener("submit", function(e) {
     e.preventDefault();
     saveData();
 }); 
 
-function populateData(data) {
+function populateForm(data) {
     document.getElementById("inputTitle").value = data.title;
     if (data.author) document.getElementById("inputAuthor").value = data.author;
     if (data.yearPublished) document.getElementById("inputYearPublished").value = data.yearPublished;
@@ -21,50 +21,137 @@ function populateData(data) {
     if (data.whenHeardAbout) document.getElementById("inputWhen").value = data.whenHeardAbout;
     if (data.notes) document.getElementById("inputNotes").value = data.notes;
     if (data.categories) document.getElementById("inputCategories").value = data.categories;
+    if (data.wantToReadScore) document.getElementById("inputWantToReadScore").value = data.wantToReadScore;
 
+    if (data.wikipediaURL) document.getElementById("inputWikipediaURL").value = data.wikipediaURL;
+    if (data.goodReadsID) document.getElementById("inputGoodReadsID").value = data.goodReadsID;
+    if (data.asin) document.getElementById("inputASIN").value = data.asin;
+
+    document.getElementById("flagRead").checked = data.flagRead;
+    document.getElementById("flagCurrentlyReading").checked = data.flagCurrentlyReading;
+    document.getElementById("flagPartiallyRead").checked = data.flagPartiallyRead;
+    document.getElementById("flagWantToRead").checked = data.flagWantToRead;
+
+    document.getElementById("ownPrint").checked = data.ownPrint;
+    document.getElementById("ownKindle").checked = data.ownKindle;
+    document.getElementById("ownKindleSample").checked = data.ownKindleSample;
+    document.getElementById("ownAudible").checked = data.ownAudible;
+    document.getElementById("ownPDF").checked = data.ownPDF;
+    document.getElementById("ownOtherAudio").checked = data.ownOtherAudio;
+
+    if (data.dateCreated) document.getElementById("inputDateCreated").value = data.dateCreated;
+
+    if (data.gR_Title) document.getElementById("hdnGR_Title").value = data.gR_Title;
+    if (data.gR_Author) document.getElementById("hdnGR_Author").value = data.gR_Author;
+    if (data.gR_Rating) document.getElementById("hdnGR_Rating").value = data.gR_Rating;
+    if (data.gR_ReviewCount) document.getElementById("hdnGR_ReviewCount").value = data.gR_ReviewCount;
+    if (data.gR_SyncDate) document.getElementById("hdnGR_SyncDate").value = data.gR_SyncDate;
+    if (data.gR_Status) document.getElementById("hdnGR_Status").value = data.gR_Status;
+    if (data.gR_StatusMessage) document.getElementById("hdnGR_StatusMessage").value = data.gR_StatusMessage;
+
+    configureLinks(data);
+}
+
+function configureLinks(data) {
+    const w = document.getElementById("linkToW");
     if (data.wikipediaURL) { 
-        const input = document.getElementById("inputWikipediaURL");
-        const a = document.getElementById("linkToW");
-
-        input.value = data.wikipediaURL;
-        a.href = data.wikipediaURL;
-        a.style.display = "block";
+        w.href = data.wikipediaURL;
+        w.style.display = "block";
+    }
+    else {
+        w.style.display = "none";
     }
 
+    const g = document.getElementById("linkToG");
     if (data.goodReadsID) { 
-        const input = document.getElementById("inputGoodReadsID");
-        const a = document.getElementById("linkToG");
         const url = `https://www.goodreads.com/book/show/${data.goodReadsID}`;
-
-        input.value = url;
-        a.href = url;
-        a.style.display = "block";
+        g.href = url;
+        g.style.display = "block";
+    }
+    else {
+        g.style.display = "none";
     }
 
+    const a = document.getElementById("linkToA");
     if (data.asin) { 
-        const input = document.getElementById("inputASIN");
-        const a = document.getElementById("linkToA");
-        input.value = data.asin;
-
         a.href = data.asin;
         a.style.display = "block";
     }
-
+    else {
+        a.style.display = "none";
+    }
 }
 
 function saveData() {
-    const formData = document.getElementById
-
     const data = {
+        title: document.getElementById("inputTitle").value,
+        author: document.getElementById("inputAuthor").value,
+        yearPublished: document.getElementById("inputYearPublished").value,
+        yearRevised: document.getElementById("inputYearRevised").value,
+        whereHeardAbout: document.getElementById("inputWhere").value,
+        whenHeardAbout: document.getElementById("inputWhen").value,
+        wikipediaURL: document.getElementById("inputWikipediaURL").value,
+        goodReadsID: document.getElementById("inputGoodReadsID").value,
+        asin: document.getElementById("inputASIN").value,
+        notes: document.getElementById("inputNotes").value,
+        categories: document.getElementById("inputCategories").value,
+        wantToReadScore: document.getElementById("inputWantToReadScore").value,
 
+        flagRead: document.getElementById("flagRead").checked,
+        flagCurrentlyReading: document.getElementById("flagCurrentlyReading").checked,
+        flagPartiallyRead: document.getElementById("flagPartiallyRead").checked,
+        flagWantToRead: document.getElementById("flagWantToRead").checked,
+
+        ownPrint: document.getElementById("ownPrint").checked,
+        ownKindle: document.getElementById("ownKindle").checked,
+        ownKindleSample: document.getElementById("ownKindleSample").checked,
+        ownAudible: document.getElementById("ownAudible").checked,
+        ownPDF: document.getElementById("ownPDF").checked,
+        ownOtherAudio: document.getElementById("ownOtherAudio").checked,
+
+        dateCreated: document.getElementById("inputDateCreated").value,
+
+        gR_Title: document.getElementById("hdnGR_Title").value,
+        gR_Author: document.getElementById("hdnGR_Author").value,
+        gR_Rating: document.getElementById("hdnGR_Rating").value,
+        gR_ReviewCount: document.getElementById("hdnGR_ReviewCount").value,
+        gR_SyncDate: document.getElementById("hdnGR_SyncDate").value,
+        gR_Status: document.getElementById("hdnGR_Status").value,
+        gR_StatusMessage: document.getElementById("hdnGR_StatusMessage").value
+
+    };
+
+    // consider switching to .fetch
+    // https://stackoverflow.com/questions/1973140/parsing-json-from-xmlhttprequest-responsejson
+
+    const xhr = new XMLHttpRequest();
+
+    if (bookID > 0) {
+        // this is an update
+        data.bookId = bookID;
+
+        xhr.open("PUT", `https://localhost:44344/api/books/${bookID}`, true);
+        xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+
+        xhr.onreadystatechange = function() {
+            if (this.readyState === XMLHttpRequest.DONE && this.status === 204) {
+                configureLinks(data);
+            }
+        };
+    }
+    else {
+        // this is a create
+        xhr.open("POST", "https://localhost:44344/api/books", true);
+        xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+        xhr.responseType = "json";
+
+        xhr.onreadystatechange = function() {
+            if (this.readyState === XMLHttpRequest.DONE && this.status === 201) {
+                const newBookID = xhr.response.bookId;
+                window.location.href = `book.html?bookID=${newBookID}`;
+            }
+        };
     }
 
-    fetch(`https://localhost:44344/api/books/${bookID}`, {
-        method: "PUT", // *GET, POST, PUT, DELETE, etc.
-        headers: { "Content-Type": "application/json; charset=utf-8" },
-        body: JSON.stringify(data), // body data type must match "Content-Type" header
-    })
-        .then(function(response) {
-            
-        });
+    xhr.send(JSON.stringify(data));
 }
--- ui/index.js
+++ ui/index.js
@@ -2,6 +2,22 @@ fetch("https://localhost:44344/api/books")
     .then(response => response.json())
     .then(createTable);
 
+// Quick and easy implementation for column sorting
+//   adapted from: https://stackoverflow.com/questions/14267781/sorting-html-table-with-javascript
+
+const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
+
+const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
+    v1 !== "" && v2 !== "" && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2))(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+
+document.querySelectorAll("th").forEach(th => th.addEventListener("click", (() => {
+    Array.from(document.querySelectorAll("tbody > tr"))
+        .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
+        .forEach(tr => document.querySelector("tbody").appendChild(tr) );
+})));
+
+// end sorting code
+
 function createTable(data) {
     const booksTable = document.getElementById("booksTableBody");
 
@@ -10,8 +26,14 @@ function createTable(data) {
 
         row.attributes["data-bookId"] = book.bookId;
 
+        const col0 = document.createElement("td");
+        col0.innerText = book.goodReadsID;
+
         const col1 = document.createElement("td");
         col1.innerText = book.title;
+        col1.title = book.title;
+        col1.classList.add("text-truncate");
+        col1.style.maxWidth = "450px";
 
         const col2 = document.createElement("td");
         col2.innerText = book.author;
@@ -19,6 +41,7 @@ function createTable(data) {
         const col3 = document.createElement("td");
         col3.innerText = book.yearPublished;
 
+        row.appendChild(col0);
         row.appendChild(col1);
         row.appendChild(col2);
         row.appendChild(col3);
