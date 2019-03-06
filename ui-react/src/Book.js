4ec8cb4cae8a3060e97a7fef48ddf01d221c0133
Jeff Ross <jross323@gmail.com>
2019-03-06 11:14:16 -0500

    routing first try

---
+++ ui-react/src/Book.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Book extends Component {
+  render() {
+    return (
+      <div>
+        <h3>This is the book detail page.</h3>
+      </div>
+    );
+  }
+}
+
+export default Book;
