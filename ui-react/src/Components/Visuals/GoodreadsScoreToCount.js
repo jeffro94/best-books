86d1530a4b8d9d94e5826154422ae8fee9d1142d
Jeff Ross <jross323@gmail.com>
2019-03-12 21:51:01 -0400

    implement basic tooltip

--- ui-react/src/Components/Visuals/GoodreadsScoreToCount.js
+++ ui-react/src/Components/Visuals/GoodreadsScoreToCount.js
@@ -1,9 +1,16 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import ReactFauxDOM from 'react-faux-dom';
 import * as d3 from "d3";
 
 class GoodreadsScoreToCount extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
+    console.log("rendering chart...");
+
     // layout constants
     const w = 700;
     const h = 400;
@@ -26,25 +33,9 @@ class GoodreadsScoreToCount extends Component {
     // define Y axis
     const yAxis = d3.axisLeft(yScale);
 
-    // define tooltip
-    /*
-    const tip = d3.tip()
-      .attr("class", 'd3-tip')
-      .html(d => ( 
-        <div className="tooltip">
-          <p>Title: { d.title }</p>
-          <p>Author: { d.author }</p>
-          <p>Rating: { d.gR_Rating}</p>
-          <p>Count: { d.gR_RatingCount }</p>
-        </div>
-      ))
-      .direction('se');
-    svg.call(tip);
-    */
-
     // create SVG element
-    const node = ReactFauxDOM.createElement("svg")
-    const svg = d3.select(node)
+    const svgNode = ReactFauxDOM.createElement("svg");
+    const svg = d3.select(svgNode)
         .attr("width", w)
         .attr("height", h);
 
@@ -55,7 +46,20 @@ class GoodreadsScoreToCount extends Component {
         .append("circle")
         .attr("cx", d => xScale(d.gR_Rating) )
         .attr("cy", d => yScale(d.gR_RatingCount) )
-        .attr("r", 3);
+        .attr("r", 4)
+        .on('mouseover', d => {
+          this.setState({
+            tooltipLeft: xScale(d.gR_Rating),
+            tooltipTop: yScale(d.gR_RatingCount),
+            tooltipOpacity: 0.8,
+            tooltipData: d
+          });
+        })
+        .on('mouseout', () => {
+          this.setState({
+            tooltipData: null
+          });
+        });
 
     // create X axis
     svg.append("g")
@@ -91,8 +95,43 @@ class GoodreadsScoreToCount extends Component {
         .attr("text-anchor", "middle")
         .classed("title", true);
 
-    return node.toReact();
+    const rootNode = ReactFauxDOM.createElement("div");
+    
+    d3.select(rootNode).attr("class", "chart");
 
+    rootNode.appendChild(svgNode);
+    rootNode.appendChild(
+      <ToolTipsy 
+        left={ this.state.tooltipLeft + 3 }
+        top={ this.state.tooltipTop + 4 }
+        opacity={ this.state.tooltipOpacity }
+        data={ this.state.tooltipData }
+        key="chart-tooltip" />
+      );
+
+    return rootNode.toReact();
+
+  }
+}
+
+class ToolTipsy extends Component {
+  render() {
+    return (
+      <div 
+        className="tooltip"
+        style={ { 
+          display: this.props.data ? "block" : "none",
+          position: "absolute",
+          top: `${this.props.top}px`,
+          left: `${this.props.left}px`
+        } }
+      >
+        <p>Title: { this.props.data ? this.props.data.title : "" }</p>
+        <p>Author: {  this.props.data ? this.props.data.author : "" }</p>
+        <p>Rating: {  this.props.data ? this.props.data.gR_Rating : "" }</p>
+        <p>Count: {  this.props.data ? this.props.data.gR_RatingCount : "" }</p>
+      </div>
+    );
   }
 }
 
